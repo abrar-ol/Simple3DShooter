7658ab87b9611e65e1efeb9fc07d0526a03fc637
Abrar Alolyan <abrar-ol@users.noreply.github.com>
2024-06-23 17:30:40 +0300

    update player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,22 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED = 10.0
+const JUMP_VELOCITY = 10.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = 20.0
+@onready var camera = $Camera3D
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * 0.005)
+		camera.rotate_x(-event.relative.y * 0.005)
+		camera.rotation.x = clamp(camera.rotation.x,-PI/4,PI/4)
+		
 
 func _physics_process(delta):
 	# Add the gravity.
