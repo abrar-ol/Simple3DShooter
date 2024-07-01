21fd2cee3fd675e855be0c9d6b7439258f31ca57
Abrar Alolyan <abrar-ol@users.noreply.github.com>
2024-07-01 15:58:38 +0300

    add the UI main menu

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,5 +1,16 @@
 extends Node3D
 
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var adress_entry = $CanvasLayer/PanelContainer/MarginContainer/VBoxContainer/AdressEntry
+
+
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+func _on_host_button_pressed():
+	pass # Replace with function body.
+
+func _on_join_button_2_pressed():
+	pass # Replace with function body.
+
