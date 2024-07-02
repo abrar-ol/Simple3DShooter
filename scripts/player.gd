26fc4e932da0a85044e5fd368102dc2a969cba23
Abrar Alolyan <abrar-ol@users.noreply.github.com>
2024-07-02 18:01:26 +0300

    add the Crosshair using raycast and fix the connection

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,11 @@
 extends CharacterBody3D
 
+# change the player layer to 2 bc when we have two players spawned
+# they are colliding infintely and move upwards 
+
+# to synchronize things between secenes: $MultiplayerSynchronizer
+# then add the properties to sync like:
+# player position and rotation, camera rotation
 
 const SPEED = 10.0
 const JUMP_VELOCITY = 10.0
@@ -9,14 +15,18 @@ var gravity = 20.0
 @onready var camera = $Camera3D
 @onready var animation_player = $AnimationPlayer
 @onready var muzzle_flash = $Camera3D/Pistol/MuzzleFlash
+@onready var ray_cast = $Camera3D/RayCast3D 
+
+var health = 3
+
 
 func _enter_tree():
 	set_multiplayer_authority(str(name).to_int())
-
 func _ready():
 	if not is_multiplayer_authority() : return
 	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	#make sure the camera set for the correct player
 	camera.current = true
 
 func _unhandled_input(event):
@@ -29,6 +39,11 @@ func _unhandled_input(event):
 	if Input.is_action_just_pressed("shoot") and \
 		animation_player.current_animation != "shoot":
 			play_shoot_effects.rpc()
+			if ray_cast.is_colliding():
+				var hit_player=ray_cast.get_collider()
+				# we may think : hit_player.health -=1
+				# but thi lowreing the helth of the local player
+				hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
 	
 func _physics_process(delta):
 	if not is_multiplayer_authority() : return
@@ -61,13 +76,23 @@ func _physics_process(delta):
 		
 	move_and_slide()
 	
+# remote procedure calls
 @rpc("call_local")
+# play_shoot_effects only called locally unliss using:
+# @rpc("call_local") to call it in remote instance and local 
 func play_shoot_effects():
 	animation_player.stop()
 	animation_player.play("shoot")
 	muzzle_flash.restart()
 	muzzle_flash.emitting = true
 
+# we calling receive_damage from the player that is dealing damage 
+@rpc("any_peer")
+func receive_damage():
+	health -=1
+	if health <= 0:
+		health=3
+		position = Vector3.ZERO
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "shoot":
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,7 +1,15 @@
 extends Node3D
 
+# $MultiplayerSpawner: uesed because with out it
+# the host contolling both client and server and both spawn in the server
+# 1- add spawn path: we are instanceing the players as a child of World node
+# 2- auto spawn list: add player secene ( what we want to spawn )
+# 3- in the player script: server and client can only control their corresponding players 
+# by set up the authority and connect it to their corresponding peer
+
 @onready var main_menu = $CanvasLayer/MainMenu
-@onready var adress_entry = $CanvasLayer/PanelContainer/MarginContainer/VBoxContainer/AdressEntry
+@onready var adress_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AdressEntry
+
 var enet_peer = ENetMultiplayerPeer.new()
 
 const PORT = 9999
@@ -15,8 +23,10 @@ func _on_host_button_pressed():
 	main_menu.hide()
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
-	multiplayer.peer_connected.connect(add_player)
-	
+	multiplayer.peer_connected.connect(add_player) #  When player press join --> add player
+	# (wire up a signal) peer_connected is a signal thet is going to emmit 
+	# at a server every time a client connect to it
+	# it connects add_player, it's gonna pass in the new peer id
 	add_player(multiplayer.get_unique_id())
 
 func _on_join_button_2_pressed():
@@ -25,6 +35,7 @@ func _on_join_button_2_pressed():
 	multiplayer.multiplayer_peer = enet_peer
 
 func add_player(peer_id):
+	print(peer_id)
 	var player = PLAYER.instantiate()
 	player.name = str(peer_id)
 	add_child(player)
